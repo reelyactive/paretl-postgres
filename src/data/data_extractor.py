e71a14fac9e15af5f4f4e3e3edd928b00481d1b8
xavier <prudentxavier@gmail.com>
2026-03-25 20:01:09 -0400

    correct indentation

--- src/data/data_extractor.py
+++ src/data/data_extractor.py
@@ -51,8 +51,8 @@ class DataExtractor:
         df = pd.DataFrame(rows, columns=colnames)
         
         if len(df) == 0:
-       		print("Extraction from database results in an empty table")
-    		sys.exit(1)
+       	    print("Extraction from database results in an empty table")
+    	    sys.exit(1)
         
         logging.info(f"[Extractor] Extracted for event {self.cfg['event_name']} {len(df)} rows from {self.cfg['source_table']} "
                      f"between {startTs} and {endTs} for the receivers {receivers}")
