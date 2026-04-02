2a6f3f0f8d926e09c3aca6cd09d2e5fee128e7ce
xavier <prudentxavier@gmail.com>
2026-04-02 15:42:31 -0400

    input database is expected to contain json data, not flat columns

--- src/data/data_extractor.py
+++ src/data/data_extractor.py
@@ -13,8 +13,8 @@ class DataExtractor:
         self.cfg = cfg
 
     def extract(self) -> pd.DataFrame:
-        # Determine time window for data extraction 
 
+        # Determine time window for data extraction 
         with self.conn.cursor() as cur:
             cur.execute(f"SELECT MIN(timestamp), MAX(timestamp) FROM {self.cfg['source_table']}")
             minTs, maxTs = cur.fetchone()
@@ -24,6 +24,8 @@ class DataExtractor:
         startTs = self.cfg.get("start_ts")  # expect ISO string or None
         endTs = self.cfg.get("end_ts")
 
+	# The ETL is either run for a (start;end) time window
+	# Or for a (now;now-frequency) time window
         if startTs and endTs:
             # Parse strings to datetime if needed
             startTs = datetime.fromisoformat(startTs) if isinstance(startTs, str) else startTs
@@ -38,10 +40,16 @@ class DataExtractor:
         receivers = self.cfg['receivers_id']  # e.g. [1, 2, 3]
         placeHolders = ",".join(["%s"] * len(receivers))
         query = f"""
-            SELECT * FROM {self.cfg['source_table']}
+            SELECT 
+            timestamp,
+            raddec->>'transmitterId' AS transmitterid,
+            raddec->>'receiverId' AS receiverid,
+            raddec->>'rssi' AS rssi
+            FROM {self.cfg['source_table']}
             WHERE timestamp BETWEEN %s AND %s
             AND raddec->>'receiverId' IN ({placeHolders})
         """
+        
         params = [startTs, endTs] + receivers
       
         with self.conn.cursor() as cur:
@@ -54,6 +62,16 @@ class DataExtractor:
         if len(df) == 0:
             print("Extraction from database results in an empty table")
             sys.exit(1)
+        
+        # Check that the columns used later are all available
+        required_cols = {"timestamp", "rssi", "transmitterid", "receiverid"}
+        actual_cols = set(df.columns)
+        missing = required_cols - actual_cols
+        if missing:
+            print(f"Error: Missing required columns in DataFrame: {', '.join(missing)}")
+            print("Expected columns derived from raddec: timestamp, rssi, transmitterid, receiverid.")
+            sys.exit(1) # Exit the script with an error code
+        
         
         logging.info(f"[Extractor] Extracted for event {self.cfg['event_name']} {len(df)} rows from {self.cfg['source_table']} "
                      f"between {startTs} and {endTs} for the receivers {receivers}")
